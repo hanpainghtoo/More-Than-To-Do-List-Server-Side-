ab89414a00b6bd6d80c399d8bc27da4216137a65
Han Paing Htoo <hanpainghtoo1@gmail.com>
2024-04-14 14:07:01 +0630

    api setup init

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', (req, res)=>{
+  res.json({
+    'More Than To DO List App': "Bone & Htoo 's Enterprise Application"
+  })
+})
+// app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +43,11 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+let port = process.env.PORT ? process.env.PORT : 3000;
+
+app.listen(port , ()=>{
+  console.log(`Server is running on http://localhost:${port}`);
+})
 
 module.exports = app;
